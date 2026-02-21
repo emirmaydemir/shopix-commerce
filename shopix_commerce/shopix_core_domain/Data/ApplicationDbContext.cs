9850cb9796f77bba7184664f4d652f83385a4a0c
Emir Muhammet Aydemir <emirmaydemir@gmail.com>
2026-02-21 18:23:37 +0300

    implement global soft delete mechanism using BaseEntity inheritance

--- shopix_commerce/shopix_core_domain/Data/ApplicationDbContext.cs
+++ shopix_commerce/shopix_core_domain/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using shopix_core_domain.Entities;
+using System.Linq.Expressions;
 
 namespace shopix_core_domain.Data
 {
@@ -18,9 +19,25 @@ namespace shopix_core_domain.Data
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }   
 
-        /*protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(builder);
-        }*/
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if(typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType).HasQueryFilter(CreateSoftDelete(entityType.ClrType));
+                }
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private LambdaExpression? CreateSoftDelete(Type clrType)
+        {
+            var parameter = Expression.Parameter(clrType, "e");
+            var property = Expression.Property(parameter, nameof(BaseEntity.IsDeleted));
+            var condition = Expression.Equal(property, Expression.Constant(false));
+            var lambda = Expression.Lambda(condition, parameter);
+            return lambda;
+        }
     }
 }
--- shopix_commerce/shopix_core_domain/Entities/BaseEntity.cs
+++ shopix_commerce/shopix_core_domain/Entities/BaseEntity.cs
@@ -12,5 +12,6 @@
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
     }
 }
